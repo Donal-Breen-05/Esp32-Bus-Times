a609d0a6b0a69d97390b6ef5a009e80244b35f9f
Donal-Breen-05 <donalbreen50@gmail.com>
2026-04-04 12:57:53 +0100

    created the main file with setup() and main loop() functions

---
+++ main.ino
@@ -0,0 +1,19 @@
+// dependencies 
+#include <WiFi.h> //core esp 
+
+//setup 
+void setup() { 
+  Serial.begin(115200); //set baud rate 
+  delay(1000); 
+
+  //title 
+  Serial.println("\n==============================");
+  Serial.println("   ESP-32 Bus Times");
+  Serial.println("==============================\n");
+
+}// end serial 
+
+//main loop 
+void loop() { 
+  //loop 
+}//end loop 
