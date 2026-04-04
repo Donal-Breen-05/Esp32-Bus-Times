ea34c4b9f08e7cdf147c9e2aebf40a7968a001ec
Donal-Breen-05 <donalbreen50@gmail.com>
2026-04-04 18:31:43 +0100

    created a function for the esp32 to connect to a wifi with the ssid and password

--- main.ino
+++ main.ino
@@ -1,6 +1,46 @@
 // dependencies 
 #include <WiFi.h> //core esp 
 
+
+//config 
+const char* wifi_SSID = "WIFINAME"; 
+const char* wifi_password = "password"; 
+
+
+
+
+void connect_wifi(){ 
+  
+  int attempts = 0 ;
+
+  //disconnect then reconnect 
+  WiFi.disconnect(true); 
+  delay(1000); 
+  WiFi.mode(WIFI_STA); 
+  delay(500); 
+  WiFi.begin(wifi_SSID, wifi_password);//connect 
+  
+  Serial.print("\nConnecting to the Wifi \"%s\"" , wifi_SSID); 
+  
+  while(WiFi.status() != WL_CONNECTED) { 
+
+    delay(500) 
+    Serial.print("\nConnecting........");
+    
+    //timeout 
+    if (++attempts > 40){ 
+      Serial.println("ERROR - could not connect"); 
+      return; 
+    }//end if     
+
+  }//end while
+  
+  //if connected
+  Serial.println("\nWiFi connected! IP: " + WiFi.localIP().toString());
+
+
+}//end connect_wifi 
+
 //setup 
 void setup() { 
   Serial.begin(115200); //set baud rate 
@@ -11,7 +51,12 @@ void setup() {
   Serial.println("   ESP-32 Bus Times");
   Serial.println("==============================\n");
 
+  //wifi 
+  connect_wifi(); 
+
 }// end serial 
+
+
 
 //main loop 
 void loop() { 
