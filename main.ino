36aa15bc63fc9dc443404410eee1b5c18f5fb4f7
Donal-Breen-05 <donalbreen50@gmail.com>
2026-04-05 23:08:26 +0100

    added bus times function to connect to the api and recive a response

--- main.ino
+++ main.ino
@@ -1,11 +1,15 @@
 // dependencies 
 #include <WiFi.h> //core esp 
-
+#include <WiFiClientSecure.h> //core esp 
+#include <ArduinoJson.h> //Library written by Benoit Blanchon
 
 //config 
 const char* wifi_SSID = "WIFINAME"; 
 const char* wifi_password = "password"; 
-
+const char* api = "https://api.nationaltransport.ie/gtfsr/v2/gtfsr"; 
+const char* key = "YOUR_API_KEY"; //replace with your own api key 
+const int* port = 443;
+const int* stop_num = 1234; //number of bus stop to track times for 
 
 
 
@@ -20,7 +24,7 @@ void connect_wifi(){
   delay(500); 
   WiFi.begin(wifi_SSID, wifi_password);//connect 
   
-  Serial.print("\nConnecting to the Wifi \"%s\"" , wifi_SSID); 
+  Serial.print("\nConnecting to the Wifi " + wifi_SSID); 
   
   while(WiFi.status() != WL_CONNECTED) { 
 
@@ -40,6 +44,43 @@ void connect_wifi(){
 
 
 }//end connect_wifi 
+
+//fetch data from the api 
+void get_bus_times(const char* api ,const char* key , const int* port){ 
+
+  //if wifi not connected 
+  if(WiFi.status() != WL_CONNECTED) {
+    Serial.println("ERROR - not connected"); 
+    return; 
+  }// end if 
+
+  Serial.println("HEAP - " + String(ESP.getFreeHeap()));
+
+  WiFiClientSecure client; 
+  client.setInsecure(); 
+
+  if (!client.connect(api , port)){ 
+    Serial.println("ERROR - connection failed"); 
+  }//end if 
+  
+  //send request 
+  String path = api + stop_num; 
+  client.println("GET " + path + " HTTP/1.0");
+  client.println("Host: " + String(api));
+  client.println("Accept: application/json");
+  client.println("Accept-Encoding: identity");
+  client.println("Connection: close");
+  client.println();
+
+  client.stop(); 
+
+  if(error) { 
+    Serial.println("ERROR - parse failed "); 
+    Serial.println("HEAP" + String(ESP.getFreeHeap()));
+  }//end if 
+  
+
+}//end bus times 
 
 //setup 
 void setup() { 
