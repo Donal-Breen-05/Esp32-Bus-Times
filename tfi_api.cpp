96fa661ebf7e51b0468b11a3a47ef89995fb7e6e
Donal Breen <donalbreen50@gmail.com>
2026-06-04 00:22:23 +0100

    rewrite of file to workaround the tfi live api instead of gtfsr

--- tfi_api.cpp
+++ tfi_api.cpp
@@ -5,6 +5,7 @@
 
 //headers 
 #include "config.h"
+#include "set_time.h"
 #include "transport_api.h"
 
 //fetch data from the api
@@ -23,47 +24,118 @@ void get_bus_times(){
   WiFiClientSecure client;
   client.setInsecure();
 
-  if (!client.connect(api , port)){
+  if (!client.connect(api_host , api_port)){
 
     Serial.println("ERROR - connection failed");
     return;
 
   }//end if
+   
+  //Get Current time 
+  String now = get_time(); 
+  
+  //request body 
+  String body =
+        "{"
+        "\"clientTimeZoneOffsetInMS\":0,"
+        "\"departureDate\":\"" + now + "\","
+        "\"departureTime\":\"" + now + "\","
+        "\"stopIds\":[\"" + String(stop_id) + "\"],"
+        "\"stopType\":\"BUS_STOP\","
+        "\"stopName\":\"" + String(stop_name) + "\","
+        "\"requestTime\":\"" + now + "\","
+        "\"departureOrArrival\":\"DEPARTURE\","
+        "\"refresh\":false"
+        "}";
 
   //send request
-  String path = "/gtfsr/v2/gtfsr?format=json";
+  client.println("POST " + String(api_path) + " HTTP/1.1");
 
-  client.println("GET " + path + " HTTP/1.0");
-  client.println("Host: " + String(api));
-  client.println("x-api-key: " + String(key));
-  client.println("Accept: application/json");
-  client.println("Accept-Encoding: identity");
+  // headers
+  client.println("Host: " + String(api_host));
+
+  //api key 
+  client.println("Ocp-Apim-Subscription-Key: " + String(api_key));
+
+  client.println("Content-Type: application/json");
+
+  //close connection 
   client.println("Connection: close");
+
+  client.print("Content-Length: ");
+
+  client.println(body.length());
+
   client.println();
 
-  //skip headers
-  unsigned long timeout = millis() + 1000;
+  // body
+  client.print(body);
 
-  while (client.connected() && millis() < timeout) {
+  client.flush();
+
+  //waits for availability 
+  while(!client.available()) {
+
+    delay(10);
+
+    if(!client.connected()) {
+
+        Serial.println(
+            "ERROR - server disconnected"
+        );
+
+        return;
+
+    }// end if 
+
+  }//end while 
+
+  
+  //check http status 
+  String status = client.readStringUntil('\n');
+
+  status.trim();
+
+  Serial.println(status);
+
+  // if bad http response
+  if(!status.startsWith("HTTP/1.1 200")){
+
+      Serial.println("ERROR - bad response");
+
+      client.stop();
+      return;
+
+  }// end if 
+  
+  //skip remaining headers 
+  while(client.connected()) {
 
     String line = client.readStringUntil('\n');
+
     line.trim();
 
-    if (line.length() == 0) break;
+    if(line.length() == 0) {
 
-  }// end while
+        break;
+    }// end if 
 
+  }// while 
+  
   delay(100);
+ 
+  StaticJsonDocument<256> filter;
 
-  //filtering the json
-  StaticJsonDocument<512> filter;
+  filter["stopDepartures"][0]["serviceNumber"] = true; // 15A , F2 , 150 etc 
+  filter["stopDepartures"][0]["destination"] = true; // dame st , charlestown ........
+  filter["stopDepartures"][0]["realTimeDeparture"] = true; 
+  filter["stopDepartures"][0]["scheduledDeparture"] = true; 
 
-  filter["entity"][0]["trip_update"]["trip"]["route_id"] = true;
-  filter["entity"][0]["trip_update"]["stop_time_update"][0]["stop_id"] = true;
-  filter["entity"][0]["trip_update"]["stop_time_update"][0]["arrival"]["time"] = true;
-
+  //json document
   StaticJsonDocument<4096> doc;
 
+
+   //parse json
   DeserializationError error =
 
     deserializeJson(
@@ -84,5 +156,37 @@ void get_bus_times(){
     return;
 
   }//end if
+  
+  
+  //print departures
+  JsonArray departures =
+    doc["stopDepartures"];
+
+  //if departures is empty 
+  if (departures.isNull()) { 
+
+    Serial.println("Error - No departures"); 
+
+  }// end if 
+
+  for(JsonObject dep : departures) {
+
+    String route = dep["serviceNumber"] | "";
+    String destination = dep["destination"] | "";
+    String departure = dep["realTimeDeparture"] | "";
+
+    if(departure == "") {
+
+      departure = dep["scheduledDeparture"] | "";
+
+    } // end if 
+
+    Serial.println(route + " -> " + departure + " (" + destination + ")" );
+
+  }// end for 
 
 }//end bus times
+
+
+
+
