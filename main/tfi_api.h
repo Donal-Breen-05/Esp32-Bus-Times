df3e9e531b6f091c5828c9e8918a914d0e9585d1
Donal Breen <donalbreen50@gmail.com>
2026-06-05 15:23:06 +0100

    changed header file to include struct

--- main/tfi_api.h
+++ main/tfi_api.h
@@ -1,6 +1,17 @@
 #ifndef TFI_API_H
 #define TFI_API_H
 
-void get_bus_times();
+
+// bus times struct 
+struct Bus { 
+
+  String route; //15A etc 
+  String numMins; // "5 mins" 
+  String destination; // "(Charlestown)"
+
+};//end Bus 
+
+int get_bus_times(Bus busArr[4]);
+
 
 #endif
