68928382ba360827bab74485fa881a86fc348961
Donal Breen <donalbreen50@gmail.com>
2026-06-05 15:30:07 +0100

    rewrite main to show the bus times returned in the struct

--- main/main.ino
+++ main/main.ino
@@ -11,35 +11,29 @@ void setup() {
   Serial.begin(115200); //set baud rate
   delay(1000);
 
-  //title
-  Serial.println("\n==============================");
-  Serial.println("   ESP-32 Bus Times");
-  Serial.println("==============================\n");
-
-  //display test 
   display_init();
-  Serial.println("displayinit"); 
-
-  display_fill(COLOR_RED);
-  delay(1000);
-
-  display_fill(COLOR_GREEN);
-  delay(1000);
-
-  display_fill(COLOR_BLUE);
-  delay(1000);
 
   display_fill(COLOR_BLACK);
 
   display_set_text_size(3);
-  display_set_text_color(COLOR_WHITE);
-
-  display_print(20, 20, "HELLO");
-
+  display_set_text_color(COLOR_ORANGE);
   
+  //title
+  display_print(10, 20, "ESP-32 Bus Times");
+  delay(1000)
+  display_clear();
+
   //wifi & time 
+  display_print(20, 20, "connecting......");
+  delay(1000)
   connect_to_wifi();
+  display_clear();
+  
+  display_print(20, 20, "Setting time......");
+  delay(1000)
   setup_time(); 
+  display_clear();
+  
 
 }// end setup
 
@@ -47,8 +41,23 @@ void setup() {
 //main loop
 void loop() {
 
-  get_bus_times();
+  //clear 
+  display_clear(); 
 
+  // struct array 
+  Bus busArr[4];
+  int count = get_bus_times(busArr);
+
+  for (int i = 0; i < count; i++) {
+
+    // print to screen the bus route , time , destination 
+    String line = busArr[i].route + " -> " + busArr[i].numMins + " (" + busArr[i].destination + ")";
+
+    display_print(10 , 20 * i , line.c_str()); 
+
+  }// end for 
+
+  //wait 30 seconds
   delay(30000);
 
-}//end loop//end loop 
+}//end loop
